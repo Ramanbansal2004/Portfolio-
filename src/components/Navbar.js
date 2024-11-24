d77c79c51d12b965ea7f30eac1b4105b4e70679f
Raman <103361956+Ramanbansal2004@users.noreply.github.com>
2024-11-24 19:36:36 +0530

    cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,17 @@ import '../Styles/Navbar.css';
 
 import { Link ,NavLink , useLocation} from 'react-router-dom'
 import logook from '../assets/Logok.png';
-const Navbar=({toggle})=>{
-  const [openMenu,setMenu]=useState('');
-  let navRef=useRef();
-  useEffect(()=>{
-    let handler=(e)=>{
-      if(!navRef.current.contains(e.target)){
-        setMenu('');
-      }  
-    }
-    document.addEventListener("click",handler);
-    return()=>{
-      document.removeEventListener("click",handler);
-    }
-  })
-  
+const Navbar=()=>{
   return(
-    <div className="navbar" ref={navRef}  style={{ backgroundColor: "#f0f2f5" }}>
+    <div className="navbar">
       <NavLink to="/"><img  className="img_1" src   ={logook}/></NavLink>
       <div className="Search">
         <input type="text" className="searchStyle" name="search" placeholder='Search..'/>
       </div>
-      <Link to="/login">Login/SignUp</Link>
+      <div className="right">
+        <Link to="/" className="gap"><i class="fa-solid fa-cart-shopping"></i></Link>
+        <Link to="/login" className="gap">Login/SignUp</Link>
+      </div>
     </div>
     )
 }
